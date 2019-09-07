b5bcdf7eaace534aaa892db3316ce271989ad7b3
Marcos Oliveira <marcos.ads.ti@gmail.com>
2019-09-07 19:53:34 -0300

    Add service app

---
+++ Thundera.Application/DataTranferObject/BaseDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Thundera.Application.DataTranferObject
+{
+    public class BaseDTO
+    {
+        [Key]
+        public Guid Id { get; set; }
+    }
+}
---
+++ Thundera.Application/Interfaces/Services/IServiceAppBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Thundera.Domain.Core.Commands;
+
+namespace Thundera.Application.Interfaces.Services
+{
+    public interface IServiceAppBase<TObject, TEntity> where TObject : class where TEntity : class
+    {
+        void Insert<TCommand>(TObject entity) where TCommand : Command, new();
+        void Update<TCommand>(TObject entity) where TCommand : Command, new();
+        void Insert(TObject entity);
+        void Update(TObject entity);
+        void Delete(Guid id);
+        TObject GetById(Guid id);
+        IEnumerable<TObject> GetAll();
+        void InsertRanger(IList<TObject> entities);
+        void UpdateRanger(IList<TObject> entities);
+        IList<THistoryData> GetAllHistory<THistoryData>(Guid id) where THistoryData : class;
+    }
+}
